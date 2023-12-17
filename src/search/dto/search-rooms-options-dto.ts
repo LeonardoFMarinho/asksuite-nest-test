9f5abc2bb129a2db6a9faa913f526f4920620cc6
LeonardoFMarinho <leonardo.fmarinho.dev@hotmail.com>
2023-12-16 22:38:22 -0300

    add class validator

--- src/search/dto/search-rooms-options-dto.ts
+++ src/search/dto/search-rooms-options-dto.ts
@@ -1,9 +1,14 @@
 import { ApiProperty } from '@nestjs/swagger';
+import { IsDateString, IsString } from 'class-validator';
 
 export class SearchRoomsOptionsDto {
   @ApiProperty({ type: String })
+  @IsString()
+  @IsDateString({ strict: true })
   checkin: string;
 
   @ApiProperty({ type: String })
+  @IsString()
+  @IsDateString({ strict: true })
   checkout: string;
 }
